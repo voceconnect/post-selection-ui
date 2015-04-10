a417a93d3be549cca606b24eb7b29c8d39a0fea2
Gary Smirny <banderon@gmail.com>
2015-04-10 16:03:23 -0400

    made code more descriptive

--- post-selection-ui.php
+++ post-selection-ui.php
@@ -83,10 +83,10 @@ class Post_Selection_UI {
 		}
 		if ( !empty($_GET['post_status']) ) {
 			$post_statuses = explode( ',', $_GET['post_status'] );
-			$allowed_statuses = array_merge(
-				array_intersect( $post_statuses, array_keys( get_post_statuses() ) ),
-				array_intersect( array( 'inherit', 'any' ), $post_statuses )
-			);
+
+			$defines_statuses = array_intersect( $post_statuses, array_keys( get_post_statuses() ) );
+			$special_statuses = array_intersect( array( 'inherit', 'any' ), $post_statuses );
+			$allowed_statuses = array_merge( $defines_statuses, $special_statuses );
 			if ( !empty( $allowed_statuses ) ) {
 				$args['post_status'] = $allowed_statuses;
 			}
